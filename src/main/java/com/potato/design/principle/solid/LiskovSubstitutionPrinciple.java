3c9a1c92ec1b4a7d240190e4405c5528e71691a0
dehuab <dehuab@tujia.com>
2019-12-11 13:54:12 +0800

    里式替换原则LSP：Liskov Substitution Principle

---
+++ src/main/java/com/potato/design/principle/solid/LiskovSubstitutionPrinciple.java
@@ -0,0 +1,16 @@
+package com.potato.design.principle.solid;
+
+/**
+ * @author dehuab
+ * 里式替换原则LSP：Liskov Substitution Principle
+ * <p>
+ * If S is a subtype of T, then objects of type T may be replaced with objects of type S, without breaking the program。
+ * Functions that use pointers of references to base classes must be able to use objects of derived classes without knowing it。
+ * </p>
+ * <p>
+ * 子类对象（object of subtype/derived class）能够替换程序（program）中父类对象（object of base/parent class）出现的任何地方，
+ * 并且保证原来程序的逻辑行为（behavior）不变及正确性不被破坏。
+ * </p>
+ */
+public class LiskovSubstitutionPrinciple {
+}
