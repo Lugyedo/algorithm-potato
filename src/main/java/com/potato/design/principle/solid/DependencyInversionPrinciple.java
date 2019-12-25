d5d2f64f7cb2cf654cecc7c3605b995e607779d0
dehuab <dehuab@tujia.com>
2019-12-25 13:54:15 +0800

    依赖反转原则(依赖倒置原则)DIP：Dependency Inversion Principle

---
+++ src/main/java/com/potato/design/principle/solid/DependencyInversionPrinciple.java
@@ -0,0 +1,17 @@
+package com.potato.design.principle.solid;
+
+/**
+ * @author dehuab
+ * 依赖反转原则(依赖倒置原则)DIP：Dependency Inversion Principle
+ * <p>
+ * High-level modules shouldn’t depend on low-level modules.
+ * Both modules should depend on abstractions. In addition, abstractions shouldn’t depend on details. Details depend on abstractions.
+ * </p>
+ * <p>
+ * 高层模块（high-level modules）不要依赖低层模块（low-level）。
+ * 高层模块和低层模块应该通过抽象（abstractions）来互相依赖。
+ * 除此之外，抽象（abstractions）不要依赖具体实现细节（details），具体实现细节（details）依赖抽象（abstractions）。
+ * </p>
+ */
+public class DependencyInversionPrinciple {
+}
