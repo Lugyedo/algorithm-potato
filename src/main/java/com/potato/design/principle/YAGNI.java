abe9fe4965ee361f1b04f0a1cf5908d5bdc8b2f6
dehuab <dehuab@tujia.com>
2019-12-26 14:33:25 +0800

    YAGNI 原则

---
+++ src/main/java/com/potato/design/principle/YAGNI.java
@@ -0,0 +1,11 @@
+package com.potato.design.principle;
+
+/**
+ * @author dehuab
+ * <p>
+ * YAGNI 原则的英文全称是：You Ain’t Gonna Need It。直译就是：你不会需要它。
+ * YAGNI 原则说的是“要不要做”的问题（当前不需要的就不要做）。
+ * </p>
+ */
+public class YAGNI {
+}
