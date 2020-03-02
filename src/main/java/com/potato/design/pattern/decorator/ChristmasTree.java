71c0eda9e74a03bbd49e5f46b81b564df073bc92
dehuab <dehuab@tujia.com>
2020-03-02 10:00:10 +0800

    装饰器模式

--- src/main/java/com/potato/design/pattern/decorator/ChristmasTree.java
+++ src/main/java/com/potato/design/pattern/decorator/ChristmasTree.java
@@ -2,7 +2,14 @@ package com.potato.design.pattern.decorator;
 
 /**
  * @author dehuab
- * https://www.baeldung.com/java-decorator-pattern
+ *
+ * <p>
+ * 树装饰 https://www.baeldung.com/java-decorator-pattern
+ * 图形装饰 https://dzone.com/articles/decorator-design-pattern-in-java
+ * </p>
+ * <p>
+ * 装饰器模式与代理模式对比 https://stackoverflow.com/questions/18618779/differences-between-proxy-and-decorator-pattern
+ * </p>
  */
 public class ChristmasTree implements Tree {
     @Override
